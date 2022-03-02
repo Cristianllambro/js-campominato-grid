a416655412acfd0118c24e6ee844c653aa0a1c86
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-02 16:48:22 +0100

    try event

--- script.js
+++ script.js
@@ -4,24 +4,33 @@ const select = document.getElementById('difficulty');
 
 
 btnPlay.addEventListener('click', function() {
-    let options = select.value;
-    container.innerHTML = '';
+    let options = select.querySelector('option');
 
-    
-})
+    if (options.value == 'easy'){
 
-let grillEasy = grill(49);
-let grillMedium = grill(81);
-let grillHard = grill(100);
+        let grillEasy = grill(49);
 
-function grill (max){
+    } else if(options.value == 'medium'){
 
-    for (let i = 1; i <= max; i++) {
-        let item = document.createElement('div');
-        item.classList.add('easy');
-        item.innerHTML = i;
-        container.append(item);
+        let grillMedium = grill(81);
+
+    } else if (options.value == 'hard') {
+
+        let grillHard = grill(100);
     }
     
-}
+    function grill (max){
+        for (let i = 1; i <= max; i++) {
+            let item = document.createElement('div');
+            item.classList.add('easy');
+            item.innerHTML = i;
+            container.append(item);
+        }
+    }
+    container.innerHTML = '';
+})
+
+
+
+
 
