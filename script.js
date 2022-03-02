f1143297e6d8327ced8f6a7a8ca2d0a4047a724a
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-02 16:30:05 +0100

    add funtion

--- script.js
+++ script.js
@@ -1,46 +1,27 @@
 const btnPlay = document.getElementById('btn-play');
-const btnReset = document.getElementById('btn-reset');
 const container = document.querySelector('.container-box');
 const select = document.getElementById('difficulty');
 
 
+btnPlay.addEventListener('click', function() {
+    let options = select.value;
+    container.innerHTML = '';
 
+    
+})
 
+let grillEasy = grill(49);
+let grillMedium = grill(81);
+let grillHard = grill(100);
 
+function grill (max){
 
-// for (let i = 1; i <= 49; i++) {
-//     let item = document.createElement('div');
-//     item.classList.add('box');
-//     item.innerHTML = i;
-//     container.append(item);
-// }
-
-
-
-select.addEventListener('change', function() {
-    let option = select.querySelector('#easy');
-
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= max; i++) {
         let item = document.createElement('div');
-        item.classList.add('box');
+        item.classList.add('easy');
         item.innerHTML = i;
         container.append(item);
     }
     
-
-
-})
-
-// select.addEventListener('change', function() {
-//     let option = select.querySelector('#medium');
-
-//     for (let i = 1; i <= 81; i++) {
-//         let item = document.createElement('div');
-//         item.classList.add('box');
-//         item.innerHTML = i;
-//         container.append(item);
-//     }
-
-
-// })
+}
 
