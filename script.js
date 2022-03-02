9fc3a7499338378df4bdf7ad8d16de1c8bb496a4
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-02 14:30:45 +0100

    starting work

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnPlay = document.getElementById('btn-play');
+const btnReset = document.getElementById('btn-reset');
+
+for (let i = 1; i <= 100; i++) {
+    let item = document.createElement('div');
+    item.classList.add('box');
+    item.innerHTML = i;
+}
