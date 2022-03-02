8aca4360b2a1fcbd21850f4d274c1e0584895e54
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-02 15:22:33 +0100

    try middle container

--- script.js
+++ script.js
@@ -3,36 +3,44 @@ const btnReset = document.getElementById('btn-reset');
 const container = document.querySelector('.container-box');
 const select = document.getElementById('difficulty');
 
-for (let i = 1; i <= 100; i++) {
-    let item = document.createElement('div');
-    item.classList.add('box');
-    item.innerHTML = i;
-    container.append(item);
-}
-
-
-for (let i = 1; i <= 81; i++) {
-    let item = document.createElement('div');
-    item.classList.add('box');
-    item.innerHTML = i;
-    container.append(item);
-}
-
-
-for (let i = 1; i <= 49; i++) {
-    let item = document.createElement('div');
-    item.classList.add('box');
-    item.innerHTML = i;
-    container.append(item);
-}
 
 
 
-select.addEventListener('change', (event) => {
-    let option = select.querySelector('option');
 
+
+// for (let i = 1; i <= 49; i++) {
+//     let item = document.createElement('div');
+//     item.classList.add('box');
+//     item.innerHTML = i;
+//     container.append(item);
+// }
+
+
+
+select.addEventListener('change', function() {
+    let option = select.querySelector('#easy');
+
+    for (let i = 1; i <= 100; i++) {
+        let item = document.createElement('div');
+        item.classList.add('box');
+        item.innerHTML = i;
+        container.append(item);
+    }
+    
 
 
 })
 
+// select.addEventListener('change', function() {
+//     let option = select.querySelector('#medium');
+
+//     for (let i = 1; i <= 81; i++) {
+//         let item = document.createElement('div');
+//         item.classList.add('box');
+//         item.innerHTML = i;
+//         container.append(item);
+//     }
+
+
+// })
 
